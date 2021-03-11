8165ef3b9d9c4bbdde0f3fa0d97f86d3597ae0d8
Usama Puri <55175411+Usamapuri@users.noreply.github.com>
2021-03-10 21:13:31 -0800

    Update 2WeekitBackend.sql

--- 2WeekitBackend.sql
+++ 2WeekitBackend.sql
@@ -50,7 +50,37 @@ CREATE TABLE task_interest(
 );
 
 
+INSERT INTO Tasks VALUES(1, 'Marathon' , 'Run a Marathon');
+INSERT INTO Tasks VALUES(2, 'Letters' , 'Write 14 letters to Loved Ones');
+INSERT INTO Tasks VALUES(3, 'Food' , 'Try food from 14 different countries');
+INSERT INTO Tasks VALUES(4, 'Tiktok' , 'Make a Tiktok Dancing Video Everyday');
+INSERT INTO Tasks VALUES(5, 'Gratitude' , 'Tell us what you are gratitude');
+INSERT INTO Tasks VALUES(6, 'Social Media' , 'Repost an insta poetry');
+INSERT INTO Tasks VALUES(7, 'Knitting Challenge', 'Knit a sweater in 14 days');
+INSERT INTO Tasks VALUES(8, 'Coding' , 'Learn to Code');
+INSERT INTO Tasks VALUES(9, 'Leet Code' , 'Do LeetCode Everyday');
+INSERT INTO Tasks VALUES(10, 'Space' , 'Space for description');
+INSERT INTO Tasks VALUES(11, 'Netflix' , 'Finish a Netflix Series');
+INSERT INTO Tasks VALUES(12, 'Gymnastics' , 'Planking Challenge');
+INSERT INTO Tasks VALUES(13, 'Videography' , 'Shoot your Story');
+INSERT INTO Tasks VALUES(14, 'Splits', 'Do some splits for everyday');
+INSERT INTO Tasks VALUES(15, 'Workout' , 'Workout Everyday for 15 days');
 
+
+INSERT INTO Interests VALUES(11, '#health');
+INSERT INTO Interests VALUES(22, '#body');
+INSERT INTO Interests VALUES(33, '#wellbeing');
+INSERT INTO Interests VALUES(44, '#sports');
+INSERT INTO Interests VALUES(55, '#friends');
+INSERT INTO Interests VALUES(66, '#amily');
+INSERT INTO Interests VALUES(77, '#food');
+INSERT INTO Interests VALUES(88, '#dance');
+INSERT INTO Interests VALUES(99, '#storytelling');
+INSERT INTO Interests VALUES(111, '#videoediting');
+INSERT INTO Interests VALUES(122, '#artsandcrafts');
+INSERT INTO Interests VALUES(133, '#computerscience');
+INSERT INTO Interests VALUES(144, '#academic');
+INSERT INTO Interests VALUES(155, '#film' );
 
 
 
